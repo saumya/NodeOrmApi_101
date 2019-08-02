78e05cd014af621a8063906b14da24713315d485
saumya <2saumyaray@gmail.com>
2019-08-02 17:48:40 +0530

    wip: APIs

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,24 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('Users API');
 });
 
+router.get('/all/', function(req, res, next) {
+  res.send('all users');
+});
+
+
+router.put('/user/', function(req, res, next) {
+  res.send('PUT user');
+});
+router.post('/user/', function(req, res, next) {
+  res.send('POST user');
+});
+router.delete('/user/', function(req, res, next) {
+  res.send('DELETE user');
+});
+
+
+//
 module.exports = router;
